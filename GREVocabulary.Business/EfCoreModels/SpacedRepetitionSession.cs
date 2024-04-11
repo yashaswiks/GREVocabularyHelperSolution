e048635e1ed12d107195ba531bb7d1d10342a98f
yashaswi <yashaswi@nabkisan.org>
2024-04-11 10:16:25 +0530

    Add Total Column to SpacedRepetitionSession Table

--- GREVocabulary.Business/EfCoreModels/SpacedRepetitionSession.cs
+++ GREVocabulary.Business/EfCoreModels/SpacedRepetitionSession.cs
@@ -5,6 +5,7 @@ public class SpacedRepetitionSession
     public int Id { get; set; }
     public int Red { get; set; }
     public int Green { get; set; }
+    public int Total { get; set; }
     public DateTime SessionTimestamp { get; set; }
     public ICollection<SessionDetail> SessionDetails { get; } = new List<SessionDetail>();
 }
---
+++ GREVocabulary.Business/Migrations/20240411044455_AddTotalColumnToSpacedRepetitionSession.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GREVocabulary.Business.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTotalColumnToSpacedRepetitionSession : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Total",
+                table: "SpacedRepetitionSessions",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Total",
+                table: "SpacedRepetitionSessions");
+        }
+    }
+}
