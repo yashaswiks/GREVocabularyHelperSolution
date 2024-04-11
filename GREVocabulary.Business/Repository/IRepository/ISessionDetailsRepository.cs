2f5f5ef1ff5ac959c96ab7ba2bf76bbe5dc28376
yashaswi <yashaswi@nabkisan.org>
2024-04-11 10:42:37 +0530

    Repositories Improved

---
+++ GREVocabulary.Business/Repository/IRepository/ISessionDetailsRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace GREVocabulary.Business.Repository.IRepository;
+
+public interface ISessionDetailsRepository
+{
+    Task<int?> InsertSessionWordAsync(int spacedRepetitionSessionId,
+        List<InsertSessionWordDetails> insertSessionWordDetails);
+}
+
+public record InsertSessionWordDetails(
+    string WordToMemorize,
+    int GroupId,
+    int Red,
+    int Green);
---
+++ GREVocabulary.Business/Repository/SpacedRepetitionSessionsRepository.cs
@@ -0,0 +1,39 @@
+﻿using Dapper;
+using GREVocabulary.Business.Repository.IRepository;
+using GREVocabulary.Business.Service.IService;
+using Microsoft.Data.Sqlite;
+using System.Data;
+
+namespace GREVocabulary.Business.Repository;
+
+public class SpacedRepetitionSessionsRepository : ISpacedRepetitionSessionsRepository
+{
+    private readonly IDatabaseOptions _databaseOptions;
+
+    public SpacedRepetitionSessionsRepository(IDatabaseOptions databaseOptions)
+    {
+        _databaseOptions = databaseOptions;
+    }
+
+    public async Task<int?> CreateSessionAsync(InsertSessionDetailsDto sessionDetails)
+    {
+        using IDbConnection _db = new SqliteConnection(_databaseOptions.ConnectionString);
+        _db.Open();
+
+        var param = new
+        {
+            Red = sessionDetails.RedCount,
+            Green = sessionDetails.GreenCount,
+            sessionDetails.SessionTimestamp,
+            Total = sessionDetails.TotalCount
+        };
+
+        var sql = @"INSERT INTO SpacedRepetitionSessions (Red, Green, SessionTimestamp, Total) 
+                        VALUES (@Red, @Green, @SessionTimestamp, @Total);
+                        SELECT last_insert_rowid();";
+
+        var newSessionId = await _db.ExecuteScalarAsync<int?>(sql, param);
+        _db.Close();
+        return newSessionId;
+    }
+}
